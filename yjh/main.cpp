d0d07ca5554294f3aabb4d8ef36986eeec59d563
yjhhhhhh <69828624+yjhhhhhh@users.noreply.github.com>
2020-10-18 11:55:41 +0800

    Update main.cpp

--- yjh/main.cpp
+++ yjh/main.cpp
@@ -1,19 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
 using namespace std;
 
 
-void helper(int N, string& table, int& table_st, priority_queue<int, vector<int>, greater<int>>& pq1, stack<int>& stk0){
-//        for(int i=N-1;i>=0;i--){
-    while(table_st!=N && (pq1.empty() || stk0.empty())){
-        if(table[table_st]=='1'){
-            pq1.push(table_st+1);
-        }
-        else if(table[table_st]=='0'){
-            stk0.push(table_st+1);
-        }
-        table_st++;
-    }
-}
 int main(){
     int T;
     cin>>T;
@@ -23,11 +14,16 @@ int main(){
         cin>>N>>table>>M>>people;
         priority_queue<int, vector<int>, greater<int> > pq1;
         stack<int> stk0;
-        int table_st = 0;
-        helper(N, table, table_st, pq1, stk0);
+        for(int i=0;i<N;i++){
+            if(table[i]=='1'){
+                pq1.push(i+1);
+            }
+            if(table[N-i-1]=='0'){
+                stk0.push(N-i-1+1);
+            }
+        }
         for(int i=0;i<M;i++){
             if(people[i] == 'M'){
-                helper(N, table, table_st, pq1, stk0);
                 if(pq1.empty()){
                     pq1.push(stk0.top());
                     stk0.pop();
@@ -39,7 +35,6 @@ int main(){
                 }
             }
             else{
-                helper(N, table, table_st, pq1, stk0);
                 if(!stk0.empty()) {
                     cout << stk0.top() << endl;
                     pq1.push(stk0.top());
