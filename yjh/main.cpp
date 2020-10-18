782f798588599ef0c58279b90df945d85a7db447
yjhhhhhh <69828624+yjhhhhhh@users.noreply.github.com>
2020-10-18 10:56:37 +0800

    Add files via upload
    meituan3

--- yjh/main.cpp
+++ yjh/main.cpp
@@ -1,63 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void op(){
-    int n, m;
-    cin>>n>>m;
-    vector<set<int>> store(n+1);
-    while(m--){
-        int a,b;
-        cin>>a>>b;
-        store[a].insert(b);
-    }
-    for(int i=1;i<=n;i++){
-        for(auto it = store[i].begin();it!=store[i].end();it++){
-            if((*it)==i)continue;
-            for(auto it2 = store[*it].begin();it2!=store[*it].end();it2++) {
-                store[i].insert(*it2);
-            }
-        }
-    }
-    vector<int> nums(n+1);
-    for(int i=1;i<=n;i++){
-        for(auto it = store[i].begin();it!=store[i].end();it++){
-            if((*it)==i)continue;
-            nums[*it]++;
-        }
-    }
-    int resbig=0,ressmall=0;
-    for(int i=1;i<=n;i++) {
-        if(nums[i]==n-1){
-            if(resbig==0){
-                resbig = i;
-            }
-            else{
-                resbig=-1;
-            }
-        }
-        if(nums[i]==0){
-            if(ressmall==0){
-                ressmall = i;
-            }
-            else{
-                ressmall=-1;
-            }
-        }
-    }
-    if(resbig==0)resbig=-1;
-    if(ressmall==0)ressmall=-1;
-    cout<<resbig<<" "<<ressmall<<endl;
-    return;
-}
-
 int main(){
-    op();
+    int T;
+    cin>>T;
+    while(T--){
+        int N, M;
+        string table,people;
+        cin>>N>>table>>M>>people;
+        priority_queue<int, vector<int>, greater<int> > pq0,pq1;
+        for(int i=0;i<N;i++){
+            if(table[i]=='1'){
+                pq1.push(i+1);
+            }
+            else if(table[i]=='0'){
+                pq0.push(i+1);
+            }
+        }
+        cout<<endl;
+        for(int i=0;i<M;i++){
+            if(people[i] == 'M'){
+                if(pq1.empty()){
+                    pq1.push(pq0.top());
+                    pq0.pop();
+                    cout<<pq1.top()<<endl;
+                }
+                else{
+                    cout<<pq1.top()<<endl;
+                    pq1.pop();
+                }
+            }
+            else{
+                if(!pq0.empty()) {
+                    cout << pq0.top() << endl;
+                    pq1.push(pq0.top());
+                    pq0.pop();
+                }
+                else{
+                    cout<<pq1.top()<<endl;
+                    pq1.pop();
+                }
+            }
+        }
+    }
     return 0;
 }
 
-//5 5
-//2 1
-//2 3
-//1 3
-//3 4
-//4 5
+//1
+//5
+//01102
+//6
+//MFMMFF
