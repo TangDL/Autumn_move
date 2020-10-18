3d72b4594df578b90ce47e8e19bc62b3e701606d
yjhhhhhh <69828624+yjhhhhhh@users.noreply.github.com>
2020-10-18 11:18:06 +0800

    Update main.cpp
    3 2

--- yjh/main.cpp
+++ yjh/main.cpp
@@ -8,21 +8,22 @@ int main(){
         int N, M;
         string table,people;
         cin>>N>>table>>M>>people;
-        priority_queue<int, vector<int>, greater<int> > pq0,pq1;
+        priority_queue<int, vector<int>, greater<int> > pq1;
+        stack<int> stk0;
         for(int i=N-1;i>=0;i--){
             if(table[i]=='1'){
                 pq1.push(i+1);
             }
             else if(table[i]=='0'){
-                pq0.push(i+1);
+                stk0.push(i+1);
             }
         }
         cout<<endl;
         for(int i=0;i<M;i++){
             if(people[i] == 'M'){
                 if(pq1.empty()){
-                    pq1.push(pq0.top());
-                    pq0.pop();
+                    pq1.push(stk0.top());
+                    stk0.pop();
                     cout<<pq1.top()<<endl;
                 }
                 else{
@@ -31,10 +32,10 @@ int main(){
                 }
             }
             else{
-                if(!pq0.empty()) {
-                    cout << pq0.top() << endl;
-                    pq1.push(pq0.top());
-                    pq0.pop();
+                if(!stk0.empty()) {
+                    cout << stk0.top() << endl;
+                    pq1.push(stk0.top());
+                    stk0.pop();
                 }
                 else{
                     cout<<pq1.top()<<endl;
