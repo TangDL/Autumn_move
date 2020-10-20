e34fe1c487beeca5296af5338ffc35170da05d88
LHT-Curry <lht13268230616@163.com>
2020-10-20 19:48:59 +0800

    10.20

--- lht/10.20.py
+++ lht/10.20.py
@@ -10,11 +10,14 @@ class Solution:
         occ = collections.defaultdict(int)
         ans = 0
         for i in range(n - minSize + 1):
-            cur = s[i : i + minSize]
+            cur = s[i: i + minSize]
             occ[cur] += 1
-            if occ[cur] >= ans and cur not in self.res:
+            if occ[cur] > ans:
+                self.res = []
                 self.res.append(cur)
                 ans = occ[cur]
+            elif occ[cur] == ans:
+                self.res.append(cur)
         return ans
 
 
@@ -23,7 +26,7 @@ k = int(input())
 s = input()
 test.maxFreq(s, k)
 res = sorted(test.res)
-res = sorted(res, key=lambda i: len(i), reverse=False)
+res = sorted(res, key=lambda i: len(i), reverse=True)
 if not res:
     print(-1)
 else:
