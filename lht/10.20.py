2a3f9a69a733d92d0f2e263882ccfc5430f6d41c
LHT-Curry <lht13268230616@163.com>
2020-10-20 19:21:41 +0800

    10.20

---
+++ lht/10.20.py
@@ -0,0 +1,27 @@
+import collections
+class Solution:
+    def __init__(self):
+        self.res = []
+
+    def maxFreq(self, s: str, minSize: int) -> int:
+        n = len(s)
+        occ = collections.defaultdict(int)
+        ans = 0
+        for i in range(n - minSize + 1):
+            cur = s[i : i + minSize]
+            occ[cur] += 1
+            if occ[cur] >= ans and cur not in self.res:
+                self.res.append(cur)
+                ans = occ[cur]
+        return ans
+
+
+test = Solution()
+k = int(input())
+s = input()
+test.maxFreq(s, k)
+res = sorted(test.res)
+if not res:
+    print(-1)
+else:
+    print(res[0])
