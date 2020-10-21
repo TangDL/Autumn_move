b618ab27fa0fb2a54f965dadc134c73736f3abbc
LHT-Curry <lht13268230616@163.com>
2020-10-21 19:50:28 +0800

    10.21(3)

---
+++ lht/10.21(3).py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(int, input().split()))
+def fun(arr, n):
+    arr2 = sorted(arr)
+    p, q = n-1, n-1
+    while p >= 0 and q >= 0:
+        if arr[p] == arr2[q]:
+            p -= 1
+            q -= 1
+        else:
+            while p >= 0 and arr[p] != arr2[q]:
+                p -= 1
+    return q+1
+print(fun(arr, n))
